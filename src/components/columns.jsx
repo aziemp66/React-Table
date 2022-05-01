cbb19a335d308d1905b3b54c3a883d9aa7e8d897
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-01 20:48:57 +0700

    Creating Header Groups

--- src/components/columns.jsx
+++ src/components/columns.jsx
@@ -35,3 +35,53 @@ export const COLUMNS = [
 		accessor: "phone",
 	},
 ];
+
+export const GROUPED_COLUMNS = [
+	{
+		Header: "Id",
+		Footer: "Id",
+		accessor: "id",
+	},
+	{
+		Header: "Name",
+		Footer: "Name",
+		columns: [
+			{
+				Header: "First Name",
+				Footer: "First Name",
+				accessor: "first_name",
+			},
+			{
+				Header: "Last Name",
+				Footer: "Last Name",
+				accessor: "last_name",
+			},
+		],
+	},
+	{
+		Header: "User Info",
+		Footer: "User Info",
+		columns: [
+			{
+				Header: "Email",
+
+				accessor: "email",
+			},
+			{
+				Header: "Age",
+
+				accessor: "age",
+			},
+			{
+				Header: "Country",
+
+				accessor: "country",
+			},
+			{
+				Header: "Phone",
+
+				accessor: "phone",
+			},
+		],
+	},
+];
